75c46607c4db7b6e073dd427c2407a5470b1e8bc
NguyenTrugnHieu <trunghieu220801@gmail.com>
2023-07-17 17:40:25 +0700

    done page 3

---
+++ Dashboard_Bootraps/index.js
@@ -0,0 +1,49 @@
+function handleMenuInfo() {
+  // if (document.querySelector(".menu-info ul").classList.contains("show")) {
+  //   document.querySelector(".menu-info ul").classList.add("hidden");
+  //   document.querySelector(".menu-info ul").classList.remove("show");
+  // } else {
+  //   document.querySelector(".menu-info ul").classList.add("show");
+  //   document.querySelector(".menu-info ul").classList.remove("hidden");
+  // }
+  document.querySelector(".menu-info ul").classList.toggle("show");
+}
+function handleDashboard() {
+  console.log("ccc");
+  document.querySelector(".dashboard-item").classList.toggle("show");
+}
+function handleBase() {
+  console.log("ccc");
+  document.querySelector(".base-item").classList.toggle("show");
+}
+function handleSidebar() {
+  console.log("ccc");
+  document.querySelector(".sidebar-item").classList.toggle("show");
+}
+function handleForms() {
+  document.querySelector(".form-item").classList.toggle("show");
+}
+function handleTables() {
+  document.querySelector(".table-item").classList.toggle("show");
+}
+function handleMaps() {
+  document.querySelector(".map-item").classList.toggle("show");
+}
+function handleCharts() {
+  document.querySelector(".charts-item").classList.toggle("show");
+}
+function handleCharts() {
+  document.querySelector(".charts-item").classList.toggle("show");
+}
+function handleEmail() {
+  document.querySelector(".email-item").classList.toggle("show");
+}
+function handleMessage() {
+  document.querySelector(".message-item").classList.toggle("show");
+}
+function handleCustomPage() {
+  document.querySelector(".page-item").classList.toggle("show");
+}
+function handleMenu() {
+  document.querySelector(".menu-item").classList.toggle("show");
+}
