1da5024da40938eaa05ae435d76b9861ed556a91
NguyenTrugnHieu <trunghieu220801@gmail.com>
2023-08-22 16:32:51 +0700

    landing page

---
+++ Pizza-LandingPage/index.js
@@ -0,0 +1,326 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
+document.body.addEventListener("mouseover", function () {
+  let element = document.querySelector(".img-welcome");
+  let element1 = document.querySelector(".title-welcome");
+  let element2 = document.querySelector(".button-demo");
+  let element3 = document.querySelector(".btn-pusrchare-slide");
+  element.classList.add("show");
+  element1.classList.add("show");
+  element2.classList.add("show");
+  element3.classList.add("show");
+});
+function scrolltoH2() {
+  let element = document.querySelector(".title-slide2");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 50;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltospan() {
+  let element = document.querySelector(".span-slide2");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 50;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoHomepage1() {
+  let element = document.querySelector(".homepage1");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 50;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoHomepage2() {
+  let element = document.querySelector(".homepage2");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 50;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoHomepage3() {
+  let element = document.querySelector(".homepage3");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 50;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoHomepage4() {
+  let element = document.querySelector(".homepage4");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 50;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoHomepage5() {
+  let element = document.querySelector(".homepage5");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 50;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoHomepage6() {
+  let element = document.querySelector(".homepage6");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 50;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide4_p() {
+  let element = document.querySelector(".slide-4 p");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide4_H1() {
+  let element = document.querySelector(".slide-4 h1");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide4_1() {
+  let element = document.querySelector(".slide4-1");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide4_2() {
+  let element = document.querySelector(".slide4-2");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide4_3() {
+  let element = document.querySelector(".slide4-3");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide4_4() {
+  let element = document.querySelector(".slide4-4");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide5_p() {
+  let element = document.querySelector(".title-slide5");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide5_H1() {
+  let element = document.querySelector(".slide-5 h1");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide5_row1() {
+  let element = document.querySelector(".slide-5 .row-1");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide5_row2() {
+  let element = document.querySelector(".slide-5 .row-2");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide6_title() {
+  let element = document.querySelector(".slide-6 .title");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide6_h1() {
+  let element = document.querySelector(".slide6-content h1");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide6_p() {
+  let element = document.querySelector(".slide6-content .text");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+
+function scrolltoSlide7_title() {
+  let element = document.querySelector(".content-container .title");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide7_h1() {
+  let element = document.querySelector(".content-container h1");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide7_card() {
+  let elements = document.querySelectorAll(
+    ".content-container .card-container"
+  );
+  let windowHeight = window.innerHeight;
+  elements.forEach((element) => {
+    let elementTop = element.getBoundingClientRect().top;
+    let elementVisible = 0;
+
+    if (elementTop < windowHeight - elementVisible) {
+      element.classList.add("show");
+    }
+  });
+}
+function scrolltoSlide8_h1() {
+  let element = document.querySelector(".slide8-container h1");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide8_p() {
+  let element = document.querySelector(".slide8-container p");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide8_btnleft() {
+  let element = document.querySelector(".button-demo1");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+function scrolltoSlide8_btnright() {
+  let element = document.querySelector(".btn-purchase1");
+  let windowHeight = window.innerHeight;
+  let elementTop = element.getBoundingClientRect().top;
+  let elementVisible = 0;
+
+  if (elementTop < windowHeight - elementVisible) {
+    element.classList.add("show");
+  }
+}
+window.addEventListener("scroll", scrolltoH2);
+window.addEventListener("scroll", scrolltospan);
+window.addEventListener("scroll", scrolltoHomepage1);
+window.addEventListener("scroll", scrolltoHomepage2);
+window.addEventListener("scroll", scrolltoHomepage3);
+window.addEventListener("scroll", scrolltoHomepage4);
+window.addEventListener("scroll", scrolltoHomepage5);
+window.addEventListener("scroll", scrolltoHomepage6);
+window.addEventListener("scroll", scrolltoSlide4_p);
+window.addEventListener("scroll", scrolltoSlide4_H1);
+window.addEventListener("scroll", scrolltoSlide4_1);
+window.addEventListener("scroll", scrolltoSlide4_2);
+window.addEventListener("scroll", scrolltoSlide4_3);
+window.addEventListener("scroll", scrolltoSlide4_4);
+window.addEventListener("scroll", scrolltoSlide5_p);
+window.addEventListener("scroll", scrolltoSlide5_H1);
+window.addEventListener("scroll", scrolltoSlide5_row1);
+window.addEventListener("scroll", scrolltoSlide5_row2);
+window.addEventListener("scroll", scrolltoSlide6_title);
+window.addEventListener("scroll", scrolltoSlide6_h1);
+window.addEventListener("scroll", scrolltoSlide6_p);
+window.addEventListener("scroll", scrolltoSlide7_title);
+window.addEventListener("scroll", scrolltoSlide7_h1);
+window.addEventListener("scroll", scrolltoSlide7_card);
+window.addEventListener("scroll", scrolltoSlide8_h1);
+window.addEventListener("scroll", scrolltoSlide8_p);
+window.addEventListener("scroll", scrolltoSlide8_btnleft);
+window.addEventListener("scroll", scrolltoSlide8_btnright);
